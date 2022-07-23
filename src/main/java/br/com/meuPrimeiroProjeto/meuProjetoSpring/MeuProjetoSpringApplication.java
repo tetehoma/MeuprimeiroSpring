051f85be7a5a5cec38730f1170a8b69d3aaf0605
tetehoma <tetehoma@gmail.com>
2022-07-23 16:57:57 -0300

    Meu primeiro Spring

---
+++ src/main/java/br/com/meuPrimeiroProjeto/meuProjetoSpring/MeuProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.meuPrimeiroProjeto.meuProjetoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meuPrimeiroProjeto/meuProjetoSpring/MeuProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuPrimeiroProjeto.meuProjetoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
